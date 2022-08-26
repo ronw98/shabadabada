f62645e9876f74adc46e782668720000f1ad5c77
rdahuron <robin.dahuron@cyim.com>
2022-08-26 19:44:36 +0200

    Add transparent system ui bars

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shabadabada/app.dart';
 import 'package:shabadabada/core/injection.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Setting SystemUIOverlay
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  //Setting SystemUIMode
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.edgeToEdge,
+    overlays: [SystemUiOverlay.top],
+  );
+
+
   setUpInjector();
   runApp(const ShabadApp());
 }
