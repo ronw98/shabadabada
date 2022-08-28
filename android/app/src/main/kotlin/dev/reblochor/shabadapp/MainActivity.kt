f8acc586a6109e0d995386618f215105fc3e3a6c
rdahuron <robin.dahuron@cyim.com>
2022-08-28 18:08:49 +0200

    Add smooth transition between android SplashScreen and flutter activity

--- android/app/src/main/kotlin/dev/reblochor/shabadapp/MainActivity.kt
+++ android/app/src/main/kotlin/dev/reblochor/shabadapp/MainActivity.kt
@@ -8,12 +8,14 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Aligns the Flutter view vertically with the window.
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             // Disable the Android splash screen fade out animation to avoid
             // a flicker before the similar frame is drawn in Flutter.
-            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                splashScreenView.animate().alpha(0f).setDuration(200).start()
+            }
         }
 
         super.onCreate(savedInstanceState)
